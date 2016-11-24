3b8f4d9aed6b75829d883b416958e5b6192b11c2
Sebastian Opałczyński <opalczynski@gmail.com>
2016-11-24 12:54:46 +0100

    [SPRINTERO REVIEW] correct as suggested;

--- core/views.py
+++ core/views.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 
-# Create your views here.
 from django.template.response import TemplateResponse
 
 
@@ -8,4 +7,10 @@ def index_view(request):
 
     return TemplateResponse(request, 'index.html', {
         'client_id': settings.SLACK_CLIENT_ID
+    })
+
+
+def success_view(request, team_domain):
+    return TemplateResponse(request, 'success.html', {
+        'team_domain': team_domain
     })
